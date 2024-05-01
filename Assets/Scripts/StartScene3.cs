4a008aa0f6b0e1b59dcf576b6efe5de50cef370b
Miguel <A01383674@tec.mx>
2024-05-01 14:41:48 -0600

    info scene

---
+++ Assets/Scripts/StartScene3.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScene3 : MonoBehaviour
+{
+    public void StartS3()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
